796968f328897e5ff2ae3f536ade57325c3ecd0d
Yap Ning Yi <nyyap002@mymail.sim.edu.sg>
2022-10-16 14:34:58 +0800

    adding r_csv file

---
+++ r_csv/code.R
@@ -0,0 +1,13 @@
+library(rvest)
+library(dplyr)
+
+website <- read_html('https://en.wikipedia.org/wiki/Comma-separated_values') # Reads in the source code
+scraped_table <- website %>% html_elements('.wikitable') %>% html_table() %>% .[[1]]
+scraped_table
+
+# Writing to csv
+write.csv(scraped_table, file = 'cars.csv', row.names=FALSE)
+
+# Read the csv
+cars <- read.csv('cars.csv')
+cars
